edd0e500fb9e626b18086be8afb16277efbe3240
Jonatas Lizandro <jonatas.lizandro@meioambiente.mg.gov.br>
2019-11-22 10:06:52 -0200

    frontend - fix meetups reducer

--- frontend/src/store/modules/meetups/reducer.js
+++ frontend/src/store/modules/meetups/reducer.js
@@ -1,7 +1,7 @@
 export default function meetups(state = [], action) {
   switch (action.type) {
     case 'MEETUPS/ADD_SUCCESS':
-      return [...state, ...action.payload];
+      return [...action.payload];
     default:
       return state;
   }
