69ff5cc740e891babce30ad6b37007aee93d1e71
Jonatas Lizandro <jonatas.exe@gmail.com>
2020-06-04 17:51:13 -0300

    frontend - signup form integrated with backend

--- frontend/src/pages/Signup/index.js
+++ frontend/src/pages/Signup/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
+import { toast } from 'react-toastify';
 import * as Yup from 'yup';
+
+import api from '~/services/api';
 
 import Button from '~/components/Button';
 import { Container, Box } from './styles';
@@ -18,13 +21,25 @@ const schema = Yup.object().shape({
 });
 
 export default function Signup() {
+  const history = useHistory();
+
+  async function onSubmit(data) {
+    try {
+      await api.post('/users', data);
+      toast.success('Cadastro realizado com sucesso');
+      history.push('/login');
+    } catch (e) {
+      toast.error('Houve uma falha ao efetuar o cadastro');
+    }
+  }
+
   return (
     <Container>
       <Box>
         <span>
           <img src={logo} alt="MeetApp" />
         </span>
-        <Form className="with-validation" schema={schema}>
+        <Form className="with-validation" schema={schema} onSubmit={onSubmit}>
           <Input name="name" placeholder="Nome completo" />
           <Input name="email" placeholder="Digite seu e-mail" />
           <Input
