9e95683181592b6bd7302154b838c22b53d087e9
Jonatas Lizandro <jonatas.exe@gmail.com>
2020-06-05 09:50:59 -0300

    frontend - implements history

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { Router } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 
 import Routes from './routes';
 import GlobalStyle from './styles/global';
 
 import store from '~/store';
+import history from './history';
 
 function App() {
   return (
     <Provider store={store}>
-      <BrowserRouter>
+      <Router history={history}>
         <Routes />
         <GlobalStyle />
         <ToastContainer autoClose={3000} />
-      </BrowserRouter>
+      </Router>
     </Provider>
   );
 }
