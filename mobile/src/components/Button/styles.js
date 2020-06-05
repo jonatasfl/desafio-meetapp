05a675d2d09bfdc5145f5c39b09ac81761ea8f15
Jonatas Lizandro <jonatas.exe@gmail.com>
2020-06-05 19:48:04 -0300

    mobile - change button from touchableopacity to rectbutton

--- mobile/src/components/Button/styles.js
+++ mobile/src/components/Button/styles.js
@@ -1,7 +1,7 @@
-import styled from "styled-components";
-import { TouchableOpacity } from "react-native-gesture-handler";
+import styled from 'styled-components';
+import { RectButton } from 'react-native-gesture-handler';
 
-export const Container = styled(TouchableOpacity)`
+export const Container = styled(RectButton)`
   width: 100%;
   height: 40px;
   align-items: center;
