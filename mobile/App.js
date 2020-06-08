a9ebe4c48595ce63810a13a3eba7eaba7b4dad40
Jonatas Lizandro <jonatas.exe@gmail.com>
2020-06-08 19:12:41 -0300

    mobile - fix redux config

--- mobile/App.js
+++ mobile/App.js
@@ -1,17 +1,19 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { StatusBar } from 'react-native';
 
 import Routes from './src/routes';
+import store from './src/store';
 
 export default function App() {
   return (
-    <>
+    <Provider store={store}>
       <StatusBar
         barStyle="light-content"
         backgroundColor="transparent"
         translucent
       />
       <Routes />
-    </>
+    </Provider>
   );
 }
--- mobile/src/screens/Login/index.js
+++ mobile/src/screens/Login/index.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
 import { useNavigation } from '@react-navigation/native';
 
 import GradientBg from '../../components/GradientBg';
@@ -7,12 +8,21 @@ import Button from '../../components/Button';
 import { Container, Logo, TextLink } from './styles';
 
 import logo from '../../assets/logo.png';
+// import { loginRequest } from '../../store/modules/user/actions';
 
 export default function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   const navigation = useNavigation();
+  const dispatch = useDispatch();
 
   function goToCadastro() {
     navigation.navigate('Cadastro');
+  }
+
+  async function handleSubmit() {
+    console.log('Enviou!', email, password);
   }
 
   return (
@@ -24,13 +34,19 @@ export default function Login() {
         keyboardType="email-address"
         autoCapitalize="none"
         returnKeyType="next"
+        value={email}
+        onChangeText={setEmail}
       />
       <Input
         placeholder="Sua senha secreta"
         secureTextEntry
         returnKeyType="send"
+        value={password}
+        onChangeText={setPassword}
       />
-      <Button height={50}>Entrar</Button>
+      <Button height={50} onPress={handleSubmit}>
+        Entrar
+      </Button>
       <TextLink onPress={goToCadastro}>Criar conta grátis</TextLink>
     </Container>
   );
