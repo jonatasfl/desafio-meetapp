41173b5a520392ed74ff9378c359593ac414e664
Jonatas Lizandro <jonatas.exe@gmail.com>
2020-06-10 20:07:57 -0300

    mobile - create profile screen (without api)

--- mobile/src/screens/MeuPerfil/index.js
+++ mobile/src/screens/MeuPerfil/index.js
@@ -1,12 +1,30 @@
-import React from "react";
-import { Text } from "react-native";
+import React from 'react';
+import { Text } from 'react-native';
 
-import { Container } from "./styles";
+import GradientBg from '~/components/GradientBg';
+import Input from '~/components/Input';
+import Button from '~/components/Button';
+
+import { Container, Form, Divider } from './styles';
 
 export default function MeuPerfil() {
   return (
     <Container>
-      <Text>Meu Perfil</Text>
+      <GradientBg colors={['#22202C', '#402845']} />
+      <Form>
+        <Input value="Jonatas Lizandro" />
+        <Input value="jonatas.exe@gmail.com" />
+        <Divider />
+        <Input placeholder="Senha atual" secureTextEntry />
+        <Input placeholder="Nova senha" secureTextEntry />
+        <Input
+          placeholder="Confirmação de senha"
+          secureTextEntry
+          returnKeyType="send"
+        />
+        <Button height={50}>Salvar perfil</Button>
+        <Button height={50}>Sair do Meetapp</Button>
+      </Form>
     </Container>
   );
 }
--- mobile/src/screens/MeuPerfil/styles.js
+++ mobile/src/screens/MeuPerfil/styles.js
@@ -1,7 +1,19 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const Container = styled.View`
   flex: 1;
-  justify-content: center;
   align-items: center;
+  padding: 0 20px;
+`;
+
+export const Form = styled.View`
+  width: 100%;
+  margin-top: 20px;
+`;
+
+export const Divider = styled.View`
+  width: 100%;
+  border-color: rgba(255, 255, 255, 0.1);
+  border-width: 0.5px;
+  margin: 20px 0;
 `;
