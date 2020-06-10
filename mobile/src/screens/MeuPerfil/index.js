ad16628bee12d1e718e5a3b47fea34838cc4766f
Jonatas Lizandro <jonatas.exe@gmail.com>
2020-06-10 20:55:30 -0300

    mobile - fix jsconfig.json

--- mobile/src/screens/MeuPerfil/index.js
+++ mobile/src/screens/MeuPerfil/index.js
@@ -1,19 +1,31 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { useState, useEffect } from 'react';
 
 import GradientBg from '~/components/GradientBg';
 import Input from '~/components/Input';
 import Button from '~/components/Button';
 
+import { getUserData } from '~/services/auth';
+
 import { Container, Form, Divider } from './styles';
 
 export default function MeuPerfil() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+
+  useEffect(() => {
+    (async () => {
+      const { email: userEmail, name: userName } = await getUserData();
+      setName(userName);
+      setEmail(userEmail);
+    })();
+  }, []);
+
   return (
     <Container>
       <GradientBg colors={['#22202C', '#402845']} />
       <Form>
-        <Input value="Jonatas Lizandro" />
-        <Input value="jonatas.exe@gmail.com" />
+        <Input value={name} />
+        <Input value={email} />
         <Divider />
         <Input placeholder="Senha atual" secureTextEntry />
         <Input placeholder="Nova senha" secureTextEntry />
