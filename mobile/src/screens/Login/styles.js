5251ca8e5ea21c5dd16f9c4ce55666e36bbf0395
Jonatas Lizandro <jonatas.exe@gmail.com>
2020-06-11 16:48:50 -0300

    mobile - keyboard avoiding view in login screen

--- mobile/src/screens/Login/styles.js
+++ mobile/src/screens/Login/styles.js
@@ -1,6 +1,10 @@
 import styled from 'styled-components';
+import { Platform } from 'react-native';
 
-export const Container = styled.View`
+export const Container = styled.KeyboardAvoidingView.attrs({
+  enabled: Platform.OS === 'ios',
+  behavior: 'padding',
+})`
   flex: 1;
   align-items: center;
   justify-content: center;
